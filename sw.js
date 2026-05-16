1ec0a6e50def8049bf53d1df067d18bde4e6400a
infinitymoviestarproduction-a11y <infinitymoviestarproduction@gmail.com>
2026-05-16 01:58:20 -0700

    Update sw.js
    Add service worker for offline functionality

--- sw.js
+++ sw.js
@@ -1,19 +1,29 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("ee-cache").then(cache => {
-      return cache.addAll([
-        "/",
-        "/admin.html",
-        "/style.css"
-      ]);
-    })
+// Service Worker for Ernest Embassy PWA
+const CACHE_NAME = 'ernest-embassy-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/visa.html',
+  '/movie.html',
+  '/tutoring.html',
+  '/education.html',
+  '/booking.html',
+  '/contact.html',
+  '/admin.html',
+  '/style.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
